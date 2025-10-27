008210f8c1e5152c7c86b8e9963c9e545bca95ee
dheeman <dheemansa2007@gmail.com>
2025-10-27 23:28:47 +0530

    feat: update conform.nvim configuration with new formatters and options

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,15 +2,25 @@ return {
 	{
 		"stevearc/conform.nvim",
 		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "williamboman/mason.nvim" },
 		opts = {
 			formatters_by_ft = {
 				lua = { "stylua" },
-				python = { "black" },
+				python = { "ruff_format", "black", stop_after_first = true },
+				toml = { "taplo" },
+				markdown = { "prettier" },
+				json = { "prettier" },
+			},
+
+			formatters = {
+				prettier = {
+					append_args = { "--prose-wrap", "always", "--print-width", "100" },
+				},
 			},
 
 			format_on_save = {
 				-- These options will be passed to conform.format()
-				timeout_ms = 5000,
+				timeout_ms = 500,
 				lsp_format = "fallback",
 			},
 			notify_no_formatters = true,

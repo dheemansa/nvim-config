f601ebb86fd76d2bf3f92dc9f9c39a98c1176479
dheeman <dheemansa2007@gmail.com>
2025-10-27 23:28:23 +0530

    feat: update colorscheme plugins and configuration

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,32 +1,16 @@
 return {
 	{
-		"nyoom-engineering/oxocarbon.nvim",
-		enabled = true,
-		lazy = false,
-		priority = 1000,
-	},
-	{
 		"folke/tokyonight.nvim",
 		lazy = true,
-		enabled = true,
+		enabled = false,
 		priority = 1000,
 		opts = {
-			transparent = true,
+			--transparent = true,
 		},
 	},
+
 	{
-		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Ensure it loads first
-		opts = {
-			options = {
-				transparency = true,
-			},
-		},
-	},
-	{ "dasupradyumna/midnight.nvim", lazy = false, priority = 1000 },
-	{
-		"norcalli/nvim-colorizer.lua",
-		event = "BufReadPost",
-		opts = {},
+		"nyoom-engineering/oxocarbon.nvim",
+		priority = 1000,
 	},
 }

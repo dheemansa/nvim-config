b592a6fdc235ef858ae54ed53a7286f8c00de292
dheeman <dheemansa2007@gmail.com>
2025-10-27 23:31:15 +0530

    chore: update treesitter.lua configuration

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,34 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = 'master',
-        event = { "BufReadPost", "BufNewFile" },
-        --lazy = false,
-        build = ":TSUpdate",
-        opts = {
-                ensure_installed = {
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "python",
-                    "javascript",
-                    "html",
-                    "css",
-                    "json",
-                    "markdown",
-                    "bash"
-                },
-                sync_install = false,
-                auto_install = true, -- Automatically install parsers for new filetypes
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = true },
-            }
-    }
+	{
+
+		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		event = { "BufReadPost", "BufNewFile" },
+		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"python",
+				"javascript",
+				"html",
+				"css",
+				"json",
+				"markdown",
+				"bash",
+			},
+			sync_install = false,
+			auto_install = true, -- Automatically install parsers for new filetypes
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 }

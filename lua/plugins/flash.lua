ebc0c04592972cf9dbf9956a9543c7e17bb0f96f
dheeman <dheemansa2007@gmail.com>
2025-11-07 23:40:10 +0530

    fix: typo in config

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,26 +2,25 @@ return {
 	"folke/flash.nvim",
 	--event = "VeryLazy",
 	lazy = true,
-	opts = {},
+	opts = {
+		--jump = { autojump = true },
+		label = {
+			style = "overlay",
+		},
+		-- disable flash override of f,F,t,T
+		modes = {
+			char = {
+				enabled = false,
+			},
+		},
+	},
+
+    -- stylua: ignore
 	keys = {
-		{
-			--might consider using <leader><leader>
-			"gs",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
-			desc = "Flash (gs)",
-		},
-		{
-			"gS",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter (gS)",
-		},
-		--{ "r", mode = { "o", "x" }, function() require("flash").remote() end, desc = "Remote Flash" },
+        --might consider using <leader><leader> 
+		{ "gs", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash (gs)", },
+		{ "gS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter (gS)", },
+		--{ "r", mode = { "o", "x" }, function() require("flash").remote() end, desc = "Remote Flash", },
 		--{ "R", mode = { "o", "x" }, function() require("flash").treesitter_remote() end, desc = "Remote Flash Treesitter" },
 	},
 }
